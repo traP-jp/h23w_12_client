7cabc6143ba869ed92a09372fd29a729c18e39ae
iChemy <controlyuu.dev@gmail.com>
2023-12-16 23:03:46 +0900

    del some

--- src/fetch-apis/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from "./GetMeResponseFromTraq";
-export * from "./GetOAuthRedirectResponse";
-export * from "./GetPingResponce";
-export * from "./HTTPValidationError";
-export * from "./LocationInner";
-export * from "./ValidationError";
